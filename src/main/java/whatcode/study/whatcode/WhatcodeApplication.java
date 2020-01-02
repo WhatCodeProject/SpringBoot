1e9a89b85dfe49a78a217da65fa262a878272e33
DongMyeongLee <dongmyeong.lee22@gmail.com>
2020-01-02 20:24:15 +0900

    first commit

---
+++ src/main/java/whatcode/study/whatcode/WhatcodeApplication.java
@@ -0,0 +1,13 @@
+package whatcode.study.whatcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/whatcode/study/whatcode/WhatcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package whatcode.study.whatcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
