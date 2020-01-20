0aaeb8de52c2e9a2ed3cec4f64be04133c7f62a7
DongMyeongLee <dongmyeong.lee22@gmail.com>
2020-01-20 20:14:56 +0900

    What Code 테이블 구조 설계

--- src/main/java/whatcode/study/whatcode/WhatcodeApplication.java
+++ src/main/java/whatcode/study/whatcode/WhatcodeApplication.java
@@ -2,7 +2,9 @@ package whatcode.study.whatcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class WhatcodeApplication {
 
---
+++ src/main/java/whatcode/study/whatcode/domain/code/Code.java
@@ -0,0 +1,42 @@
+package whatcode.study.whatcode.domain.code;
+
+import lombok.*;
+import whatcode.study.whatcode.domain.common.BaseTimeEntity;
+import whatcode.study.whatcode.domain.team.Team;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Code extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "code_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    private CodeType codeType;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    @Builder
+    public Code(String title, CodeType codeType, String content, Team team) {
+        this.title = title;
+        this.codeType = codeType;
+        this.content = content;
+        this.team = team;
+    }
+}
