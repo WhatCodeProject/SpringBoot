0b388c6740f65e14c0a2b9cddc50efe224740551
bluenight12 <ekehd258@gmail.com>
2020-02-14 16:12:20 +0900

    save

--- src/main/java/whatcode/study/whatcode/domain/member/memberController/MemberController.java
+++ src/main/java/whatcode/study/whatcode/domain/member/memberController/MemberController.java
@@ -1,4 +1,11 @@
 package whatcode.study.whatcode.domain.member.memberController;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import whatcode.study.whatcode.domain.member.memberService.MemberService;
+
+@RequiredArgsConstructor
+@RestController
 public class MemberController {
+    private final MemberService memberService;
 }
--- src/main/java/whatcode/study/whatcode/domain/member/memberService/MemberService.java
+++ src/main/java/whatcode/study/whatcode/domain/member/memberService/MemberService.java
@@ -1,4 +1,16 @@
 package whatcode.study.whatcode.domain.member.memberService;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import whatcode.study.whatcode.domain.member.dto.MemberSaveRequestDto;
+import whatcode.study.whatcode.domain.member.member.MemberRepository;
+
+@RequiredArgsConstructor
+@Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long save(MemberSaveRequestDto requestDto) { return memberRepository.save(requestDto.toEntity()).getId(); }
 }
