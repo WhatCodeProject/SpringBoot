42cf0fa77d2284aa39798f2346abe0be5438d138
DongmyeongLee <dongmyeong.lee22@gmail.com>
2020-02-19 19:01:56 +0900

    MockMvc 테스트 추가

--- src/main/java/whatcode/study/whatcode/domain/team/TeamController.java
+++ src/main/java/whatcode/study/whatcode/domain/team/TeamController.java
@@ -1,5 +1,6 @@
 package whatcode.study.whatcode.domain.team;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +16,27 @@ public class TeamController {
     private final TeamService teamService;
 
     @PostMapping("/api/team/save")
-    public ResponseEntity save(@RequestBody TeamSaveRequestDto requestDto){
+    public ResponseEntity save(@RequestBody TeamSaveRequestDto requestDto) {
 
         Long result = teamService.save(requestDto);
-        if(result>0){
-            return new ResponseEntity<>(result, HttpStatus.OK);
+
+        if (result > 0) {
+            // 이 부분 따로 json설정 안할꺼면 클래스에 감싸서 보내야해요
+            // 안그러면 리턴값이 그냥 1 이런식으로 리턴됩니다.
+            // 테스트에서 찍히는 출력 비교해서 확인해보세요
+//            return new ResponseEntity<>(new Temp(result), HttpStatus.OK);
+            return ResponseEntity.ok(new Temp(result));
         } else {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Data
+    static class Temp {
+        Long id;
+
+        public Temp(Long id) {
+            this.id = id;
         }
     }
 
--- src/test/java/whatcode/study/whatcode/domain/team/TeamApiTest.java
+++ src/test/java/whatcode/study/whatcode/domain/team/TeamApiTest.java
@@ -1,15 +1,15 @@
 package whatcode.study.whatcode.domain.team;
 
-import org.junit.After;
-import org.junit.Before;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import whatcode.study.whatcode.domain.member.MemberRepository;
 import whatcode.study.whatcode.domain.member.MemberService;
 import whatcode.study.whatcode.domain.member.dtos.MemberSaveRequestDto;
@@ -19,19 +19,17 @@ import whatcode.study.whatcode.domain.team.dtos.TeamSaveRequestDto;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 @Transactional
 class TeamApiTest {
-    private final String port = "8081";
-
     @Autowired
     EntityManager em;
-
-    @Autowired
-    TestRestTemplate restTemplate;
 
     @Autowired
     TeamRepository teamRepository;
@@ -48,14 +46,13 @@ class TeamApiTest {
     @Autowired
     MemberTeamRepository memberTeamRepository;
 
-    @After
-    public void tearDown() throws Exception{
-        teamRepository.deleteAll();
-        memberRepository.deleteAll();
-        memberTeamRepository.deleteAll();
-    }
+    @Autowired
+    MockMvc mockMvc;
 
-    @Before
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
     void settingMember(){
         String memberEmail = "test01@gmail.com";
         MemberSaveRequestDto memberDto = getMemberSaveRequestDto(memberEmail);
@@ -63,7 +60,7 @@ class TeamApiTest {
     }
 
     @Test
-    void teamSave() throws Exception {
+    void teamSaveMock() throws Exception {
 
         //given
         String memberEmail = "test01@gmail.com";
@@ -71,18 +68,23 @@ class TeamApiTest {
 
         TeamSaveRequestDto teamDto = getTeamSaveRequestDto(memberEmail, teamName);
 
-        //when
-        String url = "http://localhost:" + port + "/api/team/save";
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, teamDto, Long.class);
+        // when && than
+        /* mockMvc로 api요청 가능합니다.
+            - mock으로 하면 한번에 테스트 수행할 수 있고 해당 요청 결과 print로도 찍을 수 있어서 편할거에요. 고려해보세요
+         */
+        this.mockMvc.perform(post("/api/team/save")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(teamDto)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value("1"));
+    }
 
-        //than
-        assertThat(responseEntity.getStatusCode())
-                .isEqualTo(HttpStatus.OK);
-
-        Team resTeam = teamRepository.getOne(responseEntity.getBody());
-        assertThat(resTeam.getTeamName())
-                .isEqualTo(teamName);
-
+    @AfterEach
+    void tearDown() throws Exception{
+        teamRepository.deleteAll();
+        memberRepository.deleteAll();
+        memberTeamRepository.deleteAll();
     }
     private MemberSaveRequestDto getMemberSaveRequestDto(String email) {
         return MemberSaveRequestDto.builder()
